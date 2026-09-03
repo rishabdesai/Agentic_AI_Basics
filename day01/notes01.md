f1586b794400f6f97e21f6044971655fd03cbb96
Rishabh Desai <desai.rishab@gmail.com>
2026-09-03 15:03:13 +0530

    day1-program1.py added)

--- day01/notes01.md
+++ day01/notes01.md
@@ -5,4 +5,13 @@
 
 ### Program 1 — 
 **File:** [program1.py](program1.py)
+- Basic langchain program - static prompt
 
+
+
+``` 
+- requirement: local ollama (models: llama3.2:3b & qwen3.5:0.8b)
+- vsCode
+- Python 3.11.12
+- Langchain (pip install langchain langchain-ollama langchian-openai)
+```
---
+++ day01/program1.py
@@ -0,0 +1,18 @@
+# basic langchain program to use local model
+
+##pip install langchain langchain-ollama langchain-openai
+
+#import the required packages
+from langchain_ollama import ChatOllama
+
+#create the instance of ChatOllama
+llm = ChatOllama(model="llama3.2:3b")
+
+#user prompt
+prompt = "What is the capital of India?"
+
+#response from the model
+response = llm.invoke(prompt)
+
+#print the response
+print(response.content)
